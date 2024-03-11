8e46ae54dd3de3ed6419177c15caa8ee86927657
Daniel <danieltmo3@gmail.com>
2024-03-11 15:29:51 -0400

    first commit

---
+++ extractmethod/Calculadora.java
@@ -0,0 +1,16 @@
+public class Calculadora {
+
+    public static void main(String[] args) {
+        int[] numeros = {10, 5, 8, 12, 7};
+
+        int soma = 0;
+        for (int numero : numeros) {
+            soma += numero;
+        }
+
+        double media = (double) soma / numeros.length;
+
+        System.out.println("A soma dos números é: " + soma);
+        System.out.println("A média dos números é: " + media);
+    }
+}
