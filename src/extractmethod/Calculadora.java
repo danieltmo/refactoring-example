ae6751ec2c84d87bbb1da470768a2dd8ab161bfe
Daniel <danieltmo3@gmail.com>
2024-03-15 18:40:33 -0400

    adição em inner class

--- src/extractmethod/Calculadora.java
+++ src/extractmethod/Calculadora.java
@@ -3,7 +3,7 @@ package extractmethod;
 public class Calculadora {
 
     public static void main(String[] args) {
-        int[] numeros = {10, 5, 8, 12, 7};
+        int[] numeros = { 10, 5, 8, 12, 7 };
 
         int soma = calcularSoma(numeros);
         double media = calcularMedia(soma, numeros.length);
@@ -22,6 +22,24 @@ public class Calculadora {
 
     private static double calcularMedia(int soma, int quantidade) {
         return (double) soma / quantidade;
+    }
+
+    class InnerCalculator {
+
+        public static void wellcome() {
+            String userName = "John";
+            int userAge = 30;
+
+            // Gerando mensagem de boas-vindas
+            String message = "Olá, " + userName + "! Bem-vindo ao nosso programa.";
+            if (userAge >= 18) {
+                message += " Esperamos que você aproveite sua experiência conosco!";
+            } else {
+                message += " Pedimos desculpas, mas este programa é para maiores de 18 anos.";
+            }
+
+            System.out.println(message);
+        }
     }
 
 }
