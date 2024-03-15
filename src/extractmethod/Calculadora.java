5d1286e18f37a594710ca988623e39f44b75cb98
Daniel <danieltmo3@gmail.com>
2024-03-15 18:39:32 -0400

    extract method 1

--- src/extractmethod/Calculadora.java
+++ src/extractmethod/Calculadora.java
@@ -5,14 +5,23 @@ public class Calculadora {
     public static void main(String[] args) {
         int[] numeros = {10, 5, 8, 12, 7};
 
-        int soma = 0;
-        for (int numero : numeros) {
-            soma += numero;
-        }
-
-        double media = (double) soma / numeros.length;
+        int soma = calcularSoma(numeros);
+        double media = calcularMedia(soma, numeros.length);
 
         System.out.println("A soma dos números é: " + soma);
         System.out.println("A média dos números é: " + media);
     }
+
+    private static int calcularSoma(int[] numeros) {
+        int soma = 0;
+        for (int numero : numeros) {
+            soma += numero;
+        }
+        return soma;
+    }
+
+    private static double calcularMedia(int soma, int quantidade) {
+        return (double) soma / quantidade;
+    }
+
 }
